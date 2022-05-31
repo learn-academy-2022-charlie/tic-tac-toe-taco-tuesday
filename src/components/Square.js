bdc1475381623d6f494d219f2d7342f06bf55865
jennerdulce <jennerdulce@gmail.com>
2022-05-31 16:26:45 -0700

    changed value of squares and alternate between x and o

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 class Square extends Component {
+
+  handleClick = () => {
+    this.props.handleGameLogic(this.props.index)
+  }
+
   render() {
     return(
       <>
-        <div className="square"></div>
+        <div 
+        className="square"
+        onClick={this.handleClick}
+        >{this.props.value}
+        </div>
       </>
     )
   }
